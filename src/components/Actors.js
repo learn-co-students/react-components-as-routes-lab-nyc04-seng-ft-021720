0cdf6aeb972461af362b2c8bf2611e19ece760a7
Chris Leja <christopher.leja@gmail.com>
2020-04-29 16:59:37 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const handleActors = () => {
+    return actors.map((actor => {
+      console.log(actor)
+      return (
+      <div className="actor">
+        {actor.name}`
+        <ul> {actor.movies.map(movie => {
+          return (
+            <li>{movie}</li>
+          )
+        })
+      }
+      </ul>
+      </div>
+    )}
+    ))
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {handleActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+const handleDirectors = () => {
+  return directors.map(director => {
+    return (
+      <div>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map(movie=>{
+            return <li>{movie}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+}
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {handleDirectors()}
     </div>
   );
 }
